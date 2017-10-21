16110fb85afd11481ace84404fefeae464c5d3e1
Jonathan Camenisch <jcamenisch@users.noreply.github.com>
2017-10-21 18:36:29 -0400

    BUGFIX: Don't assume that Rails.logger is defined
    Sometimes in the before_configuration context on Rails 5, there is
    no Rails.logger

--- lib/env_bang-rails.rb
+++ lib/env_bang-rails.rb
@@ -6,11 +6,19 @@ class ENV_BANG
       File.join(Rails.root.to_s, 'config/env.rb')
     end
 
+    def warn(msg)
+      if Rails.logger
+        Rails.logger.warn(msg)
+      else
+        puts "WARNING: #{msg}"
+      end
+    end
+
     config.before_configuration do
       if File.exists?(env_rb_file)
         load env_rb_file
       else
-        Rails.logger.warn "ENV! could not find your environment variable configuration. Please create #{env_rb_file} to set up environment variables at Rails boot."
+        warn("ENV! could not find your environment variable configuration. Please create #{env_rb_file} to set up environment variables at Rails boot.")
       end
     end
   end
