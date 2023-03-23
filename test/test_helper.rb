a202c5aacc0c2b77d90ffa911bbf342db61353b5
Jonathan Camenisch <jonathan@camenisch.net>
2023-03-23 06:09:03 -0400

    HOUSEKEEPING: Get coverage report working
    * Post webhook to Coveralls when all uploads are finished
    * Add HTML formatter to aid local viewing of coverage
    * Start SimpleCov before any tests load--and thus before any tested classes are loaded

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
-require 'minitest/autorun'
-require 'minitest/spec'
-
 require 'simplecov'
 
-SimpleCov.start 'rails' do
+SimpleCov.start do
   if ENV['CI']
     require 'simplecov-lcov'
 
@@ -12,10 +9,15 @@ SimpleCov.start 'rails' do
       c.single_report_path = 'coverage/lcov.info'
     end
 
-    formatter SimpleCov::Formatter::LcovFormatter
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::LcovFormatter,
+    ])
   end
 
-  add_filter %w[version.rb initializer.rb]
+  add_filter %w[version.rb env_bang-rails.rb]
 end
 
+require 'minitest/autorun'
+require 'minitest/spec'
 require 'env_bang'
